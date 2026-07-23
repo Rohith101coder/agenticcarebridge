453a20a97e46b1530f27745ece1a05dd8306124a
Rohith101coder <rohithvadla07@gmail.com>
2026-07-23 12:01:58 +0530

    backend url updated

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://carebridgebackend-urxs.onrender.com",
+  baseURL: "https://carebridgebackend1.onrender.com",
 });
 
 export default api;
